3dcb2c7b188071024246a932554b2f7d937c0d09
Aida Beguliyeva <aidabeguliyeva@Aidas-MacBook-Air.local>
2021-12-14 21:06:17 -0800

    added additional query

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -48,4 +48,28 @@ WHERE t.to_date = '9999-01-01'
 AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'	
 ORDER BY e.emp_no;
 	
+-- Count eligible mentors born in 1965
+SELECT COUNT(emp_no)
+FROM mentorship_eligibility;
 
+-- List of mentorship_eligibility
+SELECT DISTINCT on (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility_1956_1964
+FROM employees as e
+JOIN dept_emp as de
+	ON e.emp_no = de.emp_no
+JOIN titles as t
+	ON (e.emp_no = t.emp_no)
+WHERE t.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1956-01-01' AND '1964-12-31'	
+ORDER BY e.emp_no;
+
+-- Count eligible mentors born in 1956-1964
+SELECT COUNT(emp_no)
+FROM mentorship_eligibility_1956_1964;
